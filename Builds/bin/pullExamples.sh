f230d1c169458bd25c24aba74e302650a9bc6794
David A. Shamma <aymanshamma@gmail.com>
2015-03-21 01:14:59 -0700

    Closes #5 and closes #7.  The builds are not updated just yet, pending verification and testing.

---
+++ Builds/bin/pullExamples.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+cp -v ../../LaTeX/extended-abstract.pdf ../../Examples/LaTeX\ SIGCHI\ Extended\ Abstract.pdf
+cp -v ../../LaTeX/proceedings.pdf ../../Examples/LaTeX\ SIGCHI\ Proceedings\ Format.pdf
